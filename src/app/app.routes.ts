05f28b026c49f0f690afb4beebd19cb182467d3b
Mustafa Younis <don.mu@bk.ru>
2026-09-02 01:51:54 +0300

    add auth guard for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -33,41 +34,52 @@ export const routes: Routes = [
     children: [
       {
         path: 'dashboard',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/dashboard/dashboard/dashboard.component').then(
             (m) => m.DashboardComponent,
           ),
       },
+
       {
         path: 'medicines',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/medicines/medicines-list/medicines-list.component').then(
             (m) => m.MedicinesListComponent,
           ),
       },
+
       {
         path: 'categories',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/categories/categories-list/categories-list.component').then(
             (m) => m.CategoriesListComponent,
           ),
       },
+
       {
         path: 'customers',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/customers/customers-list/customers-list.component').then(
             (m) => m.CustomersListComponent,
           ),
       },
+
       {
         path: 'suppliers',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/suppliers/suppliers-list/suppliers-list.component').then(
             (m) => m.SuppliersListComponent,
           ),
       },
+
       {
         path: 'medicines/add',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/medicines/medicine-add/medicine-add.component').then(
             (m) => m.MedicineAddComponent,
@@ -76,6 +88,7 @@ export const routes: Routes = [
 
       {
         path: 'medicines/:id/edit',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/medicines/medicine-edit/medicine-edit.component').then(
             (m) => m.MedicineEditComponent,
@@ -84,6 +97,7 @@ export const routes: Routes = [
 
       {
         path: 'medicines/:id',
+        canActivate: [authGuard],
         loadComponent: () =>
           import('./features/medicines/medicine-details/medicine-details.component').then(
             (m) => m.MedicineDetailsComponent,
